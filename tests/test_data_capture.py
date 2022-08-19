54beb2346d988fa790d00a98963c44d0ac005198
Luis Papiernik <lpapiernik24@gmail.com>
2022-08-18 22:52:42 -0500

    add tests for correct use cases

--- tests/test_data_capture.py
+++ tests/test_data_capture.py
@@ -82,3 +82,71 @@ def test_between_method_raise_error_on_bad_range():
 
     with pytest.raises(OutOfRangeError):
         data_capture.greater(-1, 1001)
+
+
+def test_less_for_correct_values():
+    data_capture = DataCapture()
+    data_capture.add(3)
+    data_capture.add(9)
+    data_capture.add(3)
+    data_capture.add(4)
+    data_capture.add(6)
+
+    stats = data_capture.build_stats()
+    assert stats.less(0) == 0
+    assert stats.less(1) == 0
+    assert stats.less(2) == 0
+    assert stats.less(3) == 0
+    assert stats.less(4) == 2
+    assert stats.less(5) == 3
+    assert stats.less(6) == 3
+    assert stats.less(7) == 4
+    assert stats.less(8) == 4
+    assert stats.less(9) == 4
+    assert stats.less(10) == 5
+    assert stats.less(234) == 5
+
+
+def test_greater_for_correct_values():
+    data_capture = DataCapture()
+    data_capture.add(3)
+    data_capture.add(9)
+    data_capture.add(3)
+    data_capture.add(4)
+    data_capture.add(6)
+
+    stats = data_capture.build_stats()
+    assert stats.greater(1000) == 0
+    assert stats.greater(200) == 0
+    assert stats.greater(10) == 0
+    assert stats.greater(9) == 0
+    assert stats.greater(8) == 1
+    assert stats.greater(7) == 1
+    assert stats.greater(6) == 1
+    assert stats.greater(5) == 2
+    assert stats.greater(4) == 2
+    assert stats.greater(3) == 3
+    assert stats.greater(2) == 5
+    assert stats.greater(1) == 5
+    assert stats.greater(0) == 5
+
+
+def test_between_for_correct_values():
+    data_capture = DataCapture()
+    data_capture.add(3)
+    data_capture.add(9)
+    data_capture.add(3)
+    data_capture.add(4)
+    data_capture.add(6)
+
+    stats = data_capture.build_stats()
+    assert stats.between(500, 754) == 0
+    assert stats.between(10, 50) == 0
+    assert stats.between(9, 15) == 1
+    assert stats.between(8, 9) == 1
+    assert stats.between(8, 10) == 1
+    assert stats.between(5, 10) == 2
+    assert stats.between(2, 8) == 3
+    assert stats.between(0, 9) == 5
+    assert stats.between(1, 7) == 3
+    assert stats.between(0, 1000) == 5
